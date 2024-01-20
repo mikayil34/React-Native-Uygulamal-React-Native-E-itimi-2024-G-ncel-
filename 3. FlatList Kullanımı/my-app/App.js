64d815d8a82c62b8cf931bcf2a42ff0bff3027f5
Mikayil Aktemur <mikayil34@gmail.com>
2024-01-20 16:08:59 +0300

    3.flatlist

---
+++ 3. FlatList Kullanımı/my-app/App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,Button,FlatList } from 'react-native';
+
+export default function App() {
+  const Courses=[
+    {name:"c#",id:1  },
+   {name:"boostrap",id:2  },
+    {name:"react",id:3  },
+     {name:"html1",id:4  },
+     {name:"html2",id:5  },
+     {name:"html3",id:6  },
+     {name:"html4",id:7  },
+  ];
+
+  return (
+    <FlatList data={Courses} 
+    //horizontal
+    //showsHorizontalScrollIndicator={false}
+     style={styles.content}  renderItem={({item})=>{
+      return <Text style={styles.itemText} key={item.id} >{item.name} </Text>
+    }} /> 
+  );
+}
+
+const styles = StyleSheet.create({
+   itemText: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    fontSize: 20, 
+    marginVertical: 5,
+    marginHorizontal: 5,
+  },content:{
+    marginVertical: 22,
+  }
+});
