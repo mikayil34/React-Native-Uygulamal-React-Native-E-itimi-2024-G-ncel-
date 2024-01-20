e17bc6b4035bc94e5762a98303a99f2e2c8fb43a
Mikayil Aktemur <mikayil34@gmail.com>
2024-01-20 21:38:29 +0300

    add

---
+++ 2. React Native'e Giris/4. Navigation/my-app/src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View,Button } from 'react-native'
+import React from 'react'
+ 
+export default function HomeScreen({navigation}) {
+   
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Ana Ekran</Text>
+      <Button
+        title="Kurslar"
+        onPress={() => navigation.navigate('Kurslarım')}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
