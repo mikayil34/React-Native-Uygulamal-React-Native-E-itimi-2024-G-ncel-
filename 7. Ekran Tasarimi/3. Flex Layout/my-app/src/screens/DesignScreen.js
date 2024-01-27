9501b5081362906adcdb7b0d4dbf43bf121e56fb
Mikayil Aktemur <mikayil34@gmail.com>
2024-01-27 12:17:46 +0300

    add

---
+++ 7. Ekran Tasarimi/3. Flex Layout/my-app/src/screens/DesignScreen.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DesignScreen = () => {
+  return (
+    <View style={styles.mainView}>
+      <Text style={styles.mainText}>1</Text>
+      <Text style={styles.mainText}>2</Text>
+      <Text style={styles.mainText}>3</Text>
+    </View>
+  )
+}
+
+export default DesignScreen
+
+const styles = StyleSheet.create({
+  mainView: {
+    borderWidth: 1,
+    borderColor: "black",
+    margin: 20,
+    borderRadius: 20,
+    alignItems:"flex-end"
+  }, mainText: {
+    borderWidth: 1,
+    borderColor: "red",
+    margin: 30,
+    // marginHorizontal:30
+    // marginVertical:30
+    padding: 20,
+    //  paddingHorizontal:20,
+    //  paddingVertical:20,
+    borderRadius: 10
+  }
+})
