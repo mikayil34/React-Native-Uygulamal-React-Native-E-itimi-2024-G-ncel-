6ba74db6742b253bc0fef6fca1ef7eda6d91ec85
Mikayil Aktemur <mikayil34@gmail.com>
2024-01-30 21:18:01 +0300

    add

---
+++ 9. Kurslarim Uygulamasi/7. Kurslari Bastirma/kurslarim/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import CourseInput from './components/CourseInput';
+
+export default function App() {
+  const [modelIsVisible, setModelIsVisible] = useState(false)
+  const [courses, setCourses] = useState([])
+  const startModal = () => {
+    setModelIsVisible(true);
+  }
+  const endModal = () => {
+    setModelIsVisible(false);
+  }
+  const addCourse = (coursTitle) => {
+    setCourses((currentCourss) => [
+      ...currentCourss,
+      { text: coursTitle, id: Math.random().toString() }
+    ]);
+
+
+    endModal();
+  }
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Button title='Kurs Ekle' color='red' onPress={startModal} />
+        <CourseInput visible={modelIsVisible} onAddCourse={addCourse} onCancel={endModal} />
+        <FlatList data={courses} renderItem={({ item }) => {
+
+          return (
+          <View style={styles.courses}>
+
+            <Text>{item.text}</Text>
+
+          </View>)
+        }} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 20
+  },courses:{
+    
+  }
+});
---
+++ 9. Kurslarim Uygulamasi/7. Kurslari Bastirma/kurslarim/components/CourseInput.js
@@ -0,0 +1,79 @@
+import { Alert, Modal, StyleSheet, Text, Pressable, View, Image, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+
+const CourseInput = ({ visible, onAddCourse,onCancel }) => {
+  const [enterCoursText, setEnterCoursText] = useState('');
+  const addCoursHanler = () => {
+    onAddCourse(enterCoursText)
+    setEnterCoursText('')
+  }
+  const courseInputHandler = (enterText) => {
+    setEnterCoursText(enterText)
+  }
+const onCancelHandler=()=>{
+  setEnterCoursText('')
+  onCancel();
+}
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        visible={visible}
+      >
+        <View style={styles.inputContainer}>
+          <Image style={styles.modelImage} source={require('../assets/images/logo.jpg')} />
+          <TextInput 
+          value={enterCoursText} 
+          onChangeText={courseInputHandler}
+           style={styles.modalTextInput}
+            placeholder='Yeni Kurs Ekle'
+             />
+          <View style={styles.modalButtonContainer} >
+            <View style={styles.button}>
+              <Button color='red' title='İptal et' onPress={onCancelHandler} />
+            </View>
+            <View style={styles.button}>
+              <Button color='blue' onPress={addCoursHanler} title='Ekle' />
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+    </View>
+  )
+}
+
+export default CourseInput
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+    padding: 15
+  },
+  modalTextInput: {
+    borderWidth: 1,
+    width: '100%',
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: 'pink',
+    borderColor: 'pink'
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8
+  },
+  modalButtonContainer: {
+    flexDirection: 'row',
+    marginTop: 15
+  },
+  modelImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+    margin: 20,
+
+  }
+})
