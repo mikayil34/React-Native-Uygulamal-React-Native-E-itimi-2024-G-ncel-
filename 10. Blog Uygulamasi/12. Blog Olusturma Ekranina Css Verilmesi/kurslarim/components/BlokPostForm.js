d9581164608fbfc4dd68111a5630fadf0fe967c8
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-04 21:21:53 +0300

    add

---
+++ 10. Blog Uygulamasi/12. Blog Olusturma Ekranina Css Verilmesi/kurslarim/components/BlokPostForm.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+
+const BlokPostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('')
+
+  return (
+    <View style={styles.main}>
+      <Text style={styles.label}>Başlığı Giriniz</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>İçeriği Giriniz</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <TouchableOpacity style={styles.buttonmain}>
+        <View style={styles.buttonview}>
+          <Text style={styles.buttontext}>Kaydet</Text>
+        </View>
+
+
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default BlokPostForm
+
+const styles = StyleSheet.create({
+  main: {
+    marginTop: 10,
+  },
+  label: {
+    fontSize: 20,
+    marginLeft: 10,
+
+  },
+  input: {
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 20,
+    padding: 5,
+    fontSize: 18,
+    marginBottom: 15,
+
+  },
+  buttonview: {
+    backgroundColor: 'green',
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+
+  },
+  buttontext: {
+    color: 'white',
+    fontSize: 20,
+
+  },
+  buttonmain: {
+    padding: 30
+  }
+})
