288dddb4025daca10b06c3ce9a7799cc39cf4074
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-09 20:52:49 +0300

    add

---
+++ 11. Firebase Authentication Uygulamasi/2. Proje Acilisi ve React Navigation Kurulumu/login/screens/LonginScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LonginScreen = () => {
+  return (
+    <View>
+      <Text>LonginScreen</Text>
+    </View>
+  )
+}
+
+export default LonginScreen
+
+const styles = StyleSheet.create({})
