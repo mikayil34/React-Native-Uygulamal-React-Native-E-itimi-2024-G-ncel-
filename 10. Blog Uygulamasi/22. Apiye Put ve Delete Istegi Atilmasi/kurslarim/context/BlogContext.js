fa274e08c434d57dfcbb0bea984c0f58621d4e27
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-09 19:31:54 +0300

    add

---
+++ 10. Blog Uygulamasi/22. Apiye Put ve Delete Istegi Atilmasi/kurslarim/context/BlogContext.js
@@ -0,0 +1,66 @@
+import React, { useState, useReducer } from 'react';
+import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'get_blogpost': 
+      return  action.payload;
+    
+    case 'edit_blogpost':
+      return state.map((blogPost) => blogPost.id == action.payload.id ? action.payload : blogPost);
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = (dispatch) => {
+  return async (title, content, callBack) => {
+    const response = await jsonServer.post('/blogposts',{title,content});
+     
+    if (callBack) {
+      callBack();
+    }
+  };
+};
+
+
+
+const editBlogPost = (dispatch) => {
+  return async (title, content, id, callBack) => {
+    const response = await jsonServer.put(`/blogposts/${id}`,{title, content});
+
+    dispatch({
+      type: 'edit_blogpost', payload: {
+        title, content, id
+      }
+    });
+    if (callBack) {
+      callBack();
+    }
+  };
+
+};
+const getBlogPost = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    console.log(response.data);
+    dispatch({ type: 'get_blogpost', payload: response.data });
+  };
+
+};
+const deleteBlogPost = (dispatch) => {
+  return async (id) => {
+    const response = await jsonServer.delete(`/blogposts/${id}`);
+    dispatch({ type: 'delete_blogpost', payload: id });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost ,getBlogPost},
+  []
+);
---
+++ 10. Blog Uygulamasi/22. Apiye Put ve Delete Istegi Atilmasi/kurslarim/screens/ShowScreen.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Context } from '../context/BlogContext'
+
+const ShowScreen = ({ route }) => {
+  const { state } = useContext(Context)
+  const blogPost = state.find((blogPost) => blogPost.id === route.params.id);
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Text style={styles.label}>Başlık  </Text>
+        <Text style={styles.content}>{blogPost.title}  </Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>İçerik  </Text>
+        <Text style={styles.content}>{blogPost.content}  </Text>
+      </View>
+
+    </View>
+  )
+}
+
+export default ShowScreen
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+
+  },
+  container: {
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 30,
+    alignItems: 'center',
+    width: '90%'
+  },
+  label: {
+    fontSize: 30,
+
+  },
+  content: {
+    fontSize: 18,
+  }
+
+
+})
