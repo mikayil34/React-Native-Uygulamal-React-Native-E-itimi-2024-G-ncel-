4502521e3a8349d95262cfbb3c8d389ff981facc
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-09 22:56:30 +0300

    add

---
+++ 11. Firebase Authentication Uygulamasi/7. Login Islemi/login/screens/LonginScreen.js
@@ -0,0 +1,117 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
+import React,{useState,useEffect} from 'react'
+import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
+
+const LonginScreen = () => {
+  const [email, setEmail] = useState('');
+const [pasword, setPasword] = useState('');
+const navigate= useNavigation()
+useEffect(()=>{
+
+  auth.onAuthStateChanged(user=>{
+
+    if(user){
+      navigate.navigate('Home');
+    } 
+  })
+})
+const handlerSignUp = () => {
+  auth
+    .createUserWithEmailAndPassword(email, pasword)
+    .then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log('Kullanıcı ', user.email);
+    })
+    .catch((error) => alert(error.message));
+};
+
+const handlerLogin = () => {
+  auth
+    .signInWithEmailAndPassword(email, pasword)
+    .then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log('Kullanıcı giriş yaptı ', user.email);
+    })
+    .catch((error) => alert(error.message));
+};
+
+
+  return (
+
+    <KeyboardAvoidingView style={styles.countainer} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} 
+        placeholder='Email' 
+        value={email} 
+        onChangeText={text=>setEmail(text)}
+        />
+        <TextInput style={styles.input} 
+        placeholder='Şifre' 
+        secureTextEntry 
+        value={pasword} 
+        onChangeText={text=>setPasword(text)}
+        />
+
+      </View>
+      <View style={styles.buttonContainer} >
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText} onPress={handlerLogin}> Giriş Yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+          <Text style={styles.outlineButtonText} onPress={handlerSignUp}> Kayıt Ol</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LonginScreen
+
+const styles = StyleSheet.create({
+  countainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%',
+
+  }, input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10,
+
+  },
+  buttonContainer: {
+    width: '60%',
+    marginTop: 40
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 10,
+
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+
+  },
+  outlineButton: {
+    backgroundColor: 'white',
+    marginTop: 5,
+
+  },
+  outlineButtonText: {
+    color: '#0782F9',
+    fontSize: 16,
+    fontWeight: '700',
+  }
+
+
+})
