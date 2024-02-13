af93f6753f0524c23b9ac6a907c08d086ab391ea
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-13 21:21:29 +0300

    add

---
+++ 12. Sayi Bulmaca Uygulamasi/5. LineerGradient Ekleme/oyun/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import GameStartScreen from './secreens/GameStartScreen';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function App() {
+  return (
+    <LinearGradient style={styles.container} colors={['rgba(0,0,0,0.8)', 'transparent']}> 
+      <GameStartScreen />
+    </LinearGradient>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {  
+    flex:1,
+    
+  },
+});
