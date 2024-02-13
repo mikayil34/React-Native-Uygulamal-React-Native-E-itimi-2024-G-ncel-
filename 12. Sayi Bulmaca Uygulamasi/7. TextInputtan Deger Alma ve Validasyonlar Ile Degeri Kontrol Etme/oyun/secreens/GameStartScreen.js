e7c0893f3d9730263d72a5ec3d3eb18614f13636
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-13 21:57:04 +0300

    add

---
+++ 12. Sayi Bulmaca Uygulamasi/7. TextInputtan Deger Alma ve Validasyonlar Ile Degeri Kontrol Etme/oyun/secreens/GameStartScreen.js
@@ -0,0 +1,94 @@
+import { Alert, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import CustomButton from '../components/CustomButton'
+
+const GameStartScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState('')
+  const resetHandler = () => {
+    setEnteredNumber('');
+  }
+
+  const confirmHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Geçersiz Sayı', 'Sayı 1 ile 99 arasında olmaladır',
+        [{ text: 'onayla', style: 'destructive', onPress: resetHandler }]);
+
+    }
+
+  }
+
+  const numberHandler = (text) => {
+    setEnteredNumber(text);
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <Text>Sayı Tahmin Uygulaması</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input}
+          keyboardType='number-pad'
+          maxLength={2}
+          onChangeText={numberHandler}
+          value={enteredNumber}
+        />
+        <View style={styles.buttonsContainer} >
+          <View style={styles.buttonContainer}>
+            <CustomButton onPress={resetHandler}>Temizle</CustomButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <CustomButton onPress={confirmHandler}>Onayla</CustomButton>
+          </View>
+
+
+        </View>
+
+      </View>
+
+
+    </View>
+  )
+}
+
+export default GameStartScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+
+  }, card: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+    marginTop: 20,
+    borderRadius: 20,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+
+  }, input: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'yellow',
+    width: 50,
+    height: 50,
+    marginVertical: 10,
+    fontSize: 35,
+    fontWeight: 'bold',
+
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+
+  },
+  buttonContainer: {
+    flex: 1,
+  }
+
+})
