b86c93323d9928da4a2bff31eaa6d9b13097ee1f
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-17 09:40:15 +0300

    add

---
+++ 12. Sayi Bulmaca Uygulamasi/11. Sayi Arttirma ve Azaltma Aksiyonlari/oyun/secreens/GameScreen.js
@@ -0,0 +1,67 @@
+import { StyleSheet, Text, View, Alert } from 'react-native'
+import React, { useState } from 'react'
+import Title from '../components/Title'
+import ComputerNumber from '../components/ComputerNumber'
+import CustomButton from '../components/CustomButton'
+
+const GameScreen = ({ userNumber }) => {
+  let minNumber = 1;
+  let maxNumber = 100;
+
+  const generalNumber = (min, max, exclude) => {
+    const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+    if (exclude === randomNumber) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    } else {
+      return randomNumber;
+    }
+  }
+  const initialGaesse = generalNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGaesse);
+
+
+
+  const nextGuessHAndler = (direction) => {
+
+    if ((direction === 'lower' && currentGuess < userNumber) || (direction === 'greater' && currentGuess > userNumber)) {
+      Alert.alert('Hadi oradan!!!', 'Yanlış olduğunu bilebile Basıyorsun', [{ text: "tamam", style: 'cancel' }])
+      return;
+    }
+
+
+    if (direction === 'lower') {
+      maxNumber = currentGuess;
+
+    }
+    if (direction === 'greater') {
+      minNumber = currentGuess + 1;
+    }
+    const newRandomNumber = generalNumber(minNumber, maxNumber, currentGuess);
+    setCurrentGuess(newRandomNumber);
+  }
+  return (
+    <View style={styles.container}>
+      <Title>Bilgisayar Tahmini </Title>
+      <ComputerNumber>{currentGuess}</ComputerNumber>
+      <View>
+        <Text>Altında mı? Üstünde Mi?</Text>
+        <View>
+          <CustomButton onPress={nextGuessHAndler.bind(this, 'lower')}>-</CustomButton>
+          <CustomButton onPress={nextGuessHAndler.bind(this, 'greater')}>+</CustomButton>
+
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default GameScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  }
+})
