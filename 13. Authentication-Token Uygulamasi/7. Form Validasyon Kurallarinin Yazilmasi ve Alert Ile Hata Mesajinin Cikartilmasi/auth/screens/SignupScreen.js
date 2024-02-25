b18b295b6d878f2537d91df65c6a5a508ed9f421
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-25 08:08:00 +0300

    add

---
+++ 13. Authentication-Token Uygulamasi/7. Form Validasyon Kurallarinin Yazilmasi ve Alert Ile Hata Mesajinin Cikartilmasi/auth/screens/SignupScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AuthContent from '../components/AuthContent'
+
+const SignupScreen = () => {
+  return (
+    <View>
+      <AuthContent/>
+    </View>
+  )
+}
+
+export default SignupScreen
+
+const styles = StyleSheet.create({})
