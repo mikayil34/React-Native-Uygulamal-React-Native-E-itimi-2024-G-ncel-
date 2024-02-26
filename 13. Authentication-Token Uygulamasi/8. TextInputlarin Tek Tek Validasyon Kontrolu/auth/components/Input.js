81895400e979e2d6df124608fef2e555d5ffd9d5
Mikayil Aktemur <mikayil34@gmail.com>
2024-02-26 21:33:41 +0300

    add

--- 13. Authentication-Token Uygulamasi/8. TextInputlarin Tek Tek Validasyon Kontrolu/auth/components/Input.js
+++ 13. Authentication-Token Uygulamasi/8. TextInputlarin Tek Tek Validasyon Kontrolu/auth/components/Input.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
-export default function Input({ Label, keyboardType, onUpdateValue, value,secure ,isInvalid}) {
+export default function Input({ Label, keyboardType, onUpdateValue, value, secure, isInvalid }) {
   return (
     <View style={styles.InputContainer}>
       <Text style={styles.label}>{Label}</Text>
@@ -17,20 +17,20 @@ export default function Input({ Label, keyboardType, onUpdateValue, value,secure
 }
 
 const styles = StyleSheet.create({
-  InputContainer:{
-marginVertical:8,
+  InputContainer: {
+    marginVertical: 8,
 
-  },label:{
-color:'white',
-marginBottom:3,
+  }, label: {
+    color: 'white',
+    marginBottom: 3,
 
   },
-  input:{
-    backgroundColor:'pink',
-    paddingVertical:8,
-    paddingHorizontal:10,
-    borderRadius:20,
-    fontSize:16,
+  input: {
+    backgroundColor: 'pink',
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    fontSize: 16,
 
   }
 })
