200706a9feaf01eb3dcf457a9faa4cbc8372b3e0
Mikayil Aktemur <mikayil34@gmail.com>
2024-03-03 09:43:23 +0300

    add

---
+++ 13. Authentication-Token Uygulamasi/21. Anasayfaya Css Propertyleri Verilmesi/auth/screens/LoginScreen.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, Alert } from 'react-native'
+import React, { useState ,useContext} from 'react'
+import AuthContent from '../components/AuthContent'
+import { LoginUser } from '../util/auth'
+import Loading from '../components/Loading';
+import { AuthContext } from '../store/auth-context';
+ 
+export default function LoginScreen() {
+  const [isAuthanticating, setIsAuthanticating] = useState(false);
+  const authContext=useContext(AuthContext);
+  async function LoginUpHandler({ email, pasword }) {
+    setIsAuthanticating(true);
+    try { 
+      const token=  await LoginUser(email, pasword);
+       
+      authContext.authenticete(token);
+    } catch (error) {
+      Alert.alert('Giriş Yapılamadı!', 'Lütfen bilgilerinizi kontrol ediniz')
+    }
+
+    setIsAuthanticating(false);
+
+  }
+  if (isAuthanticating) {
+    return <Loading message="Kullancı girişi yapılıyor" />
+  }
+  return (
+    <View>
+      <AuthContent isLogin onAuthenticate={LoginUpHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
