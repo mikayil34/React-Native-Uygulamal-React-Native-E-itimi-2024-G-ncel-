3c55f91270089dff0afbfd4e405712bad89fee9c
Mikayil Aktemur <mikayil34@gmail.com>
2024-03-03 05:00:49 +0300

    add

---
+++ 13. Authentication-Token Uygulamasi/17. Kullanici Giris ve Kayit Durumlarinda Contexte Erisip Token Degeri Set Etme/auth/store/auth-context.js
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react"
+
+
+export const AuthContext = createContext({
+    token: '',
+    isAuthenticate: false,
+    authenticete: (token) => { },
+    logout: () => { }
+});
+
+function AuthContextProvider({ children }) {
+    const [authToken, setAuthToken] = useState(null)
+    function authenticete(token) {
+        console.log(token);
+        setAuthToken(token);
+    } 
+    function logout(token) {
+        setAuthToken(null);
+    }
+   
+    const value = {
+        token: authToken,
+        isAuthenticate: !!authToken,
+        authenticete: authenticete,
+        logout: logout,
+
+    }
+    return <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+
+}
+
+export default AuthContextProvider;
