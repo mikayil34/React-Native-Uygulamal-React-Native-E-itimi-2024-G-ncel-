0d09cd69c6e75b8759542c4971302a34be6210be
Mikayil Aktemur <mikayil34@gmail.com>
2024-04-14 08:09:53 +0300

    edit

---
+++ 15. Ogrenim Icerigim Uygulamasi/23. Metodlarin Props Olarak Gecilmesi/course/screens/ManageCourses.js
@@ -0,0 +1,68 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React, { useContext, useLayoutEffect } from 'react'
+import { EvilIcons } from '@expo/vector-icons';
+
+import { CoursesContext } from '../stor/CoursesContext';
+import CoursForm from '../components/CoursForm';
+
+
+export default function ManageCourses({ route, navigation }) {
+  const CoursId = route.params?.CoursId;
+  const coursesContext = useContext(CoursesContext);
+  let IsEdting = CoursId ? true : false;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+
+      title: IsEdting ? 'Kursu Güncelle' : 'Kurs Ekle',
+
+    }, [navigation, IsEdting])
+  });
+  function deleteCourse() {
+    coursesContext.deleteCourse(CoursId);
+    navigation.goBack();
+  };
+  function addorUpdateHandler(courseData) {
+    console.log(courseData);
+    
+    if (IsEdting) {
+      coursesContext.updateCourse(CoursId, courseData);
+    } else {
+      coursesContext.addCourse(courseData);
+    }
+    navigation.goBack();
+  }
+  function cancelHandler() {
+
+    navigation.goBack();
+  }
+  return (
+
+    <View style={styles.container}>
+      <CoursForm 
+      cancelHandler={cancelHandler} 
+      onSubmit={addorUpdateHandler} 
+        IsEdting={IsEdting}
+        />
+     
+      {IsEdting && <View style={styles.deleteContainer}>
+        <EvilIcons name="trash" size={36} color="black" onPress={deleteCourse} />
+      </View>}
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    padding: 25,
+  }, deleteContainer: {
+    alignItems: 'center',
+    borderTopWidth: 2,
+    borderTopColor: 'blue',
+    paddingTop: 10,
+    marginTop: 16,
+
+  },
+
+})
