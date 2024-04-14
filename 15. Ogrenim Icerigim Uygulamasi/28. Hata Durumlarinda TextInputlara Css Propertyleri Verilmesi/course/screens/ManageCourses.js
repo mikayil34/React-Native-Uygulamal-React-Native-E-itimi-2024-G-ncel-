5a49fe72d390f5494af1852335ac3431fc0b9ecc
Mikayil Aktemur <mikayil34@gmail.com>
2024-04-14 18:01:01 +0300

    edit

---
+++ 15. Ogrenim Icerigim Uygulamasi/28. Hata Durumlarinda TextInputlara Css Propertyleri Verilmesi/course/screens/ManageCourses.js
@@ -0,0 +1,58 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React, { useContext, useLayoutEffect } from 'react'
+
+
+import { CoursesContext } from '../stor/CoursesContext';
+import CoursForm from '../components/CoursForm';
+
+
+export default function ManageCourses({ route, navigation }) {
+  const CoursId = route.params?.CoursId;
+  const coursesContext = useContext(CoursesContext);
+  let IsEdting = CoursId ? true : false;
+const selectedCourse = coursesContext.courses.find(course => course.id === CoursId);
+  
+  useLayoutEffect(() => {
+    navigation.setOptions({ 
+      title: IsEdting ? 'Kursu Güncelle' : 'Kurs Ekle',
+
+    }, [navigation, IsEdting])
+  });
+  function deleteCourse() {
+    coursesContext.deleteCourse(CoursId);
+    navigation.goBack();
+  };
+  function addorUpdateHandler(courseData) {
+   
+    if (IsEdting) {
+      coursesContext.updateCourse(CoursId, courseData);
+    } else {
+      coursesContext.addCourse(courseData);
+    }
+    navigation.goBack();
+  }
+  function cancelHandler() {
+
+    navigation.goBack();
+  }
+  return (
+
+    <View style={styles.container}>
+      <CoursForm 
+      cancelHandler={cancelHandler} 
+      onSubmit={addorUpdateHandler} 
+      deleteCourse={deleteCourse}
+        IsEdting={IsEdting}
+        defaultvalue={selectedCourse}
+        /> 
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    padding: 25,
+  }
+
+})
